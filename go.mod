2d040a660d7b2407f3b070f40ffd50c0afad32f4
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-07 09:17:10 +0000

    e

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.19
+
+require fyne.io/fyne/v2 v2.3.4 // indirect
--- main.go
+++ main.go
@@ -1,65 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"math"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
-// d/dx sin(x) = lim(h->0) [sin(x+h) - sin(x)] / h
-//            = lim(h->0) [sin(x)cos(h) + cos(x)sin(h) - sin(x)] / h
-//            = lim(h->0) [cos(h) - 1] * sin(x) / h + cos(x) * lim(h->0) sin(h) / h
-//            = cos(x)
-
-// d/dx cos(x) = lim(h->0) [cos(x+h) - cos(x)] / h
-//            = lim(h->0) [cos(x)cos(h) - sin(x)sin(h) - cos(x)] / h
-//            = lim(h->0) [-sin(h)] * sin(x) / h + cos(x) * lim(h->0) [cos(h) - 1] / h
-//            = -sin(x)
-
-// d/dx tan(x) = d/dx [sin(x) / cos(x)]
-//            = [cos(x) * d/dx sin(x) - sin(x) * d/dx cos(x)] / cos^2(x)
-//            = [cos(x) * cos(x) - (-sin(x)) * sin(x)] / cos^2(x)
-//            = sec^2(x)
-
-// d/dx log_a(x) = 1 / (x * ln(a))
-// d/dx ln(x) = 1 / x
-
 func main() {
-	sinX := func(x float64) float64 {
-		return math.Sin(x)
-	}
-	cosX := differentiateTrigFunction(sinX, 0.5)
-	fmt.Printf("sin(0.5)의 도함수는 %.4f입니다.\n", cosX)
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Hello, World!")
 
-	tanX := func(x float64) float64 {
-		return math.Tan(x)
-	}
-	sec2X := differentiateTrigFunction(tanX, 0.5)
-	fmt.Printf("tan(0.5)의 도함수는 %.4f입니다.\n", sec2X)
+	hello := widget.NewLabel("Hello, World!")
+	content := container.NewVBox(hello, widget.NewButton("Quit", func() {
+		myApp.Quit()
+	}))
+	myWindow.SetContent(content)
 
-	logX := func(x float64) float64 {
-		return math.Log2(x)
-	}
-	dLogX := differentiateLogFunction(logX, 10, 2)
-	fmt.Printf("log_2(10)의 도함수는 %.4f입니다.\n", dLogX)
-
-	lnX := func(x float64) float64 {
-		return math.Log(x)
-	}
-	dLnX := differentiateLnFunction(lnX, 3)
-	fmt.Printf("ln(3)의 도함수는 %.4f입니다.\n", dLnX)
-}
-
-func differentiateTrigFunction(f func(x float64) float64, x float64) float64 {
-	h := 0.00000001
-	return (f(x+h) - f(x)) / h
-}
-
-func differentiateLogFunction(f func(x float64) float64, x float64, a float64) float64 {
-	h := 0.00000001
-	return 1 / (x*math.Log(a) + h)
-}
-
-func differentiateLnFunction(f func(x float64) float64, x float64) float64 {
-	h := 0.00000001
-	return 1 / (x + h)
+	myWindow.ShowAndRun()
 }
