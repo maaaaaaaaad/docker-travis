58f9f686badc046de78d08d0724caa074a5499de
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-31 09:54:31 +0000

    e

--- main.go
+++ main.go
@@ -6,25 +6,17 @@ func solution(e int, starts []int) []int {
 		}
 	}
 
-	maxNum := make([]int, e+1)
-	maxFreq := freq[1]
-	maxNum[1] = 1
-	for i := 2; i <= e; i++ {
-		if freq[i] >= maxFreq {
-			maxFreq = freq[i]
-			maxNum[i] = i
-		} else {
-			maxNum[i] = maxNum[i-1]
-		}
-	}
-
 	result := make([]int, len(starts))
 	for i, start := range starts {
-		if start == 1 {
-			result[i] = maxNum[e]
-		} else {
-			result[i] = maxNum[e] - maxNum[start-1]
+		maxFreq := freq[start]
+		maxNum := start
+		for j := start; j <= e; j++ {
+			if freq[j] > maxFreq {
+				maxFreq = freq[j]
+				maxNum = j
+			}
 		}
+		result[i] = maxNum
 	}
 
 	return result
